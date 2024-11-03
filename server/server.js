c76f1ae1ab21e01a2fd326d083c88f6e55f6f7a3
Anuj Tiwari <anujtiwari31135@gmail.com>
2024-11-03 22:54:11 +0530

    again Updated server.js with cors

--- server/server.js
+++ server/server.js
@@ -5,9 +5,9 @@ const UserModel = require('./models/usermodel')
 
 const app = express()
 const port = process.env.PORT || 8000;
-// app.use(express.json())
-// app.use(express.urlencoded({ extended: false }));
-// app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }));
+app.use(cors())
 
 
 
